e3f5d4395b2de6615ec77edcee8c83193b48cc16
Ahmed-Magdy28 <ahmedmagdy2849@gmail.com>
2026-03-30 23:05:09 +0200

    removed the  test data for the sign in

--- src/features/authentication/LoginForm.tsx
+++ src/features/authentication/LoginForm.tsx
@@ -1,62 +1,62 @@
-import { useState } from 'react';
-import Button from '../../ui/Button';
-import Form from '../../ui/Form';
-import Input from '../../ui/Input';
-import FormRowVertical from '../../ui/FormRowVertical';
-import useLogin from './hooks/useLogin';
-import SpinnerMini from '../../ui/SpinnerMini';
+import { useState } from "react";
+import Button from "../../ui/Button";
+import Form from "../../ui/Form";
+import Input from "../../ui/Input";
+import FormRowVertical from "../../ui/FormRowVertical";
+import useLogin from "./hooks/useLogin";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 // username: test@example.com
 // password: 123456789
 
 function LoginForm() {
-   const [email, setEmail] = useState('ahmedmagdy2849@gmail.com');
-   const [password, setPassword] = useState('123456789');
-   const { login, isLoggingIn } = useLogin();
-   function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
-      e.preventDefault();
-      if (!email || !password) return;
-      login(
-         { email, password },
-         {
-            onError: () => {
-               setPassword('');
-               setEmail('');
-            },
-         },
-      );
-   }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isLoggingIn } = useLogin();
+  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!email || !password) return;
+    login(
+      { email, password },
+      {
+        onError: () => {
+          setPassword("");
+          setEmail("");
+        },
+      },
+    );
+  }
 
-   return (
-      <Form onSubmit={handleSubmit}>
-         <FormRowVertical label="Email address">
-            <Input
-               type="email"
-               id="email"
-               // This makes this form better for password managers
-               autoComplete="username"
-               value={email}
-               onChange={(e) => setEmail(e.target.value)}
-               disabled={isLoggingIn}
-            />
-         </FormRowVertical>
-         <FormRowVertical label="Password">
-            <Input
-               type="password"
-               id="password"
-               autoComplete="current-password"
-               value={password}
-               disabled={isLoggingIn}
-               onChange={(e) => setPassword(e.target.value)}
-            />
-         </FormRowVertical>
-         <FormRowVertical>
-            <Button $size="large" disabled={isLoggingIn}>
-               {isLoggingIn ? <SpinnerMini /> : 'Login'}
-            </Button>
-         </FormRowVertical>
-      </Form>
-   );
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormRowVertical label="Email address">
+        <Input
+          type="email"
+          id="email"
+          // This makes this form better for password managers
+          autoComplete="username"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoggingIn}
+        />
+      </FormRowVertical>
+      <FormRowVertical label="Password">
+        <Input
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          disabled={isLoggingIn}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </FormRowVertical>
+      <FormRowVertical>
+        <Button $size="large" disabled={isLoggingIn}>
+          {isLoggingIn ? <SpinnerMini /> : "Login"}
+        </Button>
+      </FormRowVertical>
+    </Form>
+  );
 }
 
 export default LoginForm;
